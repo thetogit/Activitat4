6a2d3f8f67930015ba4ab065f6125e16b6fe50e0
thetogit <josepfarresf@gmail.com>
2021-02-05 09:32:23 +0100

    He modificat el arxiu

--- src/test1.java
+++ src/test1.java
@@ -4,6 +4,7 @@ public class test1 {
 	public static void main(String[] args) {
 		System.out.println("Hola món");
 		System.out.println("Me llamo pepe");
+		System.out.println("Estic modificant el arxiu");
 
 	}
 
